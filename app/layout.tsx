841d5a355c65ba2e46adadf1f43b89b90df60f0b
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2025-11-05 11:54:38 +0545

    feat: wrap NavigationBar and children in Suspense for improved loading state

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Sarpanch } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { NavigationBar } from "@/components/navigation-bar";
+import { Suspense } from "react";
 
 const inter = Inter({
   variable: "--font-inter-sans",
@@ -30,9 +31,11 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${sarpanch.variable} antialiased font-sans`}
       >
-        <NavigationBar />
-        {children}
-        <Toaster position="top-right" />
+        <Suspense fallback={<div>Loading...</div>}>
+          <NavigationBar />
+          {children}
+          <Toaster position="top-right" />
+        </Suspense>
       </body>
     </html>
   );
